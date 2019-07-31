37088205cdd0ce7f54bac7e005c3a36b9e836be5
Jorge Guerra <jorgeguerra1098@gmail.com>
2019-07-31 08:28:19 -0600

    ADD
    Inheritance.

---
+++ InterfaceAbstract/src/interfaceabstract/Ejecutivo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceabstract;
+
+/**
+ *
+ * @author cocoguerra
+ */
+public class Ejecutivo extends Empleado {
+
+    @Override
+    public void salario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void nombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void genero() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void estadoCivil() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void fechaNacimiento() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void Profesion() {
+        System.out.println("Soy Doctor");
+    }
+    
+}
---
+++ InterfaceAbstract/src/interfaceabstract/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceabstract;
+
+/**
+ *
+ * @author cocoguerra
+ */
+public interface Persona {
+    public void nombre();
+    public void genero();
+    public void estadoCivil();
+    public void fechaNacimiento();
+}
